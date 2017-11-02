9e22cbc463e3781699822da5c8550e93dff00a17
rafnid96 <33325204+rafnid96@users.noreply.github.com>
2017-11-02 09:07:48 -0700

    Add files via upload

---
+++ student/student/test.sql
@@ -0,0 +1,11 @@
+
+
+CREATE TABLE student (
+  rollno int(11) NOT NULL,
+  name varchar(255) DEFAULT NULL,
+  age int(11) DEFAULT NULL,
+  marks int(11) DEFAULT NULL,
+  PRIMARY KEY (rollno));
+
+
+
